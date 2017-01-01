97c909bfbcde0f26ece0487e06250e5300805660
kaz-yamada <kaz.yamada@hotmail.com>
2017-01-01 16:55:15 +1100

    Switched over to use jquery

--- sitescripts.js
+++
@@ -1,7 +0,0 @@
-function getAge(){
-	var todayDate = new Date();
-	var birthDate = new Date(1992,04);
-	var age = new Date();
-	
-	document.getElementById("age").innerHTML = todayDate.getFullYear()-birthDate.getFullYear();
-}
